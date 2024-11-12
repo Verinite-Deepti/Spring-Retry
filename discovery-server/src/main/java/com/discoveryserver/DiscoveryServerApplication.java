b3566026948d42b6cdff053ca8d5d103149de32b
Deeptiwani <Deeptiwani@deeptiIntern.VERINITE1.local>
2024-11-12 21:47:33 +0530

    Initial Commit.

---
+++ discovery-server/src/main/java/com/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,35 @@
+package com.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return ALWAYS_SAMPLE;
+	}
+
+	private static final Sampler ALWAYS_SAMPLE = new Sampler() {
+		@Override
+		public boolean isSampled(long traceId) {
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "AlwaysSample";
+		}
+	};
+
+}
