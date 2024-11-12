e035fe05f826fa07b2e594e382fcc2cd8a41bd7a
Deeptiwani <Deeptiwani@deeptiIntern.VERINITE1.local>
2024-11-12 22:12:42 +0530

    Initial Commit.

---
+++ api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,62 @@
+package com.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.apigateway.filters.ErrorFilter;
+import com.apigateway.filters.PostFilter;
+import com.apigateway.filters.PreFilter;
+import com.apigateway.filters.RouteFilter;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return ALWAYS_SAMPLE;
+	}
+
+	private static final Sampler ALWAYS_SAMPLE = new Sampler() {
+		@Override
+		public boolean isSampled(long traceId) {
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "AlwaysSample";
+		}
+	};
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+}
